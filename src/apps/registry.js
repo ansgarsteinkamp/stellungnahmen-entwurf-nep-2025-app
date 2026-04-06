0e04a0707f431327a1e11442f52f181f5140d825
Ansgar Steinkamp <ansgar.steinkamp@gmail.com>
2026-04-06 12:51:57 +0200

    Überflüssig durch die Entscheidung für App #1

--- src/apps/registry.js
+++
@@ -1,7 +0,0 @@
-const modules = import.meta.glob("./app*/index.jsx", { eager: true });
-
-const registry = Object.values(modules)
-   .map(m => m.default)
-   .sort((a, b) => a.id - b.id);
-
-export default registry;
